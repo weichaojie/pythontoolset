066a3bb42a5c158ac8e750c387f42612dbd4ea8d
weichaojie <18971302695@189.cn>
2017-04-30 11:18:12 +0800

    1）删除多余的pyw文件； 2）修改批处理执行方式，不再维护两份代码文件，改为自动从py文件拷贝一份pyw文件；
    Signed-off-by: weichaojie <18971302695@189.cn>

--- capture_screen_by_pil/devops/build.bat
+++ capture_screen_by_pil/devops/build.bat
@@ -1,6 +1,11 @@
+rem 运行到当前目录下
 cd /d "%~dp0"
 cd /d "%cd%\"
 
+rem 首先将py文件拷贝一份并将文件名重命名为pyw后缀
+copy ../src/capture_screen.py ../src/capture_screen.pyw
 
+rem 执行打包操作并输出编译后的文件
 pyinstaller -F -w --noconsole ../src/capture_screen.pyw --distpath=../../../output
+
 pause
