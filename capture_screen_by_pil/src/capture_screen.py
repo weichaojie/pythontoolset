f7b512a74e720fbb511710cad635080ec55c2fcd
weichaojie <18971302695@189.cn>
2017-05-01 18:23:05 +0800

    增加上传到百度云的代码 Signed-off-by: weichaojie <18971302695@189.cn>

--- capture_screen_by_pil/src/capture_screen.py
+++ capture_screen_by_pil/src/capture_screen.py
@@ -5,6 +5,30 @@ import time  # 以时间字符串作为文件名称
 import os,os.path,datetime
 from itertools import izip
 import Image
+import urllib
+import urllib2
+
+__author__ = 'Administrator'
+from poster.encode import multipart_encode
+from poster.streaminghttp import  register_openers
+
+register_openers()
+
+def upload(fileName):
+    # 通过百度开发者 API 上传文件到百度云
+    datagen, headers = multipart_encode({"file": open("E:\\PHPTest\\Test1\\%s"%fileName, "rb")})
+    baseurl = "https://pcs.baidu.com/rest/2.0/pcs/file?"
+    args = {
+        "method": "upload",
+        "access_token": "0.a2834e35964a7b0704242wef160507c1.2592000.1386326697.1060338330-1668780",
+        "path": "/apps/ResourceSharing/%s"%fileName
+    }
+    encodeargs = urllib.urlencode(args)
+    url = baseurl + encodeargs
+    print(url)
+    request = urllib2.Request(url, datagen, headers)
+    result = urllib2.urlopen(request).read()
+    print(result)
 
 # 计算两个文件的差异率，差异率越高表明图片内容的差异越大
 def calc_difference_rate(left_image, right_image):
