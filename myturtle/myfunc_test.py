a1f8401515e7fa59d5ddeb96362c54a758d9dc6c
weichaojie <weichaojie@huawei.com>
2017-10-15 22:31:29 +0800

    新增函数测试代码
    Signed-off-by: weichaojie <weichaojie@huawei.com>

---
+++ myturtle/myfunc_test.py
@@ -0,0 +1,19 @@
+#coding=utf-8
+
+#from turtle import *
+import time
+
+def my_func(strinput):
+    print("hello")
+    print(strinput)
+    for each in range(4):
+        print("forward 180")
+        print("turn right 90 degree")
+        time.sleep(1)
+#        turtle.forward(180)
+#        turtle.right(90)
+    
+    
+    
+my_func("weiluorui's mother");
+
