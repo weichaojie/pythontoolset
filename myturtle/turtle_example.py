1fbae1c236b55fbef3e4551899f2408eaea403cf
weichaojie <weichaojie@huawei.com>
2017-10-18 00:15:53 +0800

    新增turtle练习例子
    Signed-off-by: weichaojie <weichaojie@huawei.com>

---
+++ myturtle/turtle_example.py
@@ -0,0 +1,14 @@
+import turtle
+import time
+
+mypen = turtle.Pen()
+
+print("luojinrong")
+
+for each in range(4):
+    mypen.forward(180);
+    time.sleep(1);
+    mypen.right(90);
+
+mypen.right(180)
+mypen.forward(90)
