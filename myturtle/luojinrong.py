e1c8c5fa8b44fae09e3c5007ccc135762b85b8d1
weichaojie <weichaojie@huawei.com>
2017-11-18 16:39:18 +0800

    更新绘图代码
    Signed-off-by: weichaojie <weichaojie@huawei.com>

--- myturtle/luojinrong.py
+++ myturtle/luojinrong.py
@@ -4,23 +4,16 @@ import time
 import random
 import math
 
+print(u"武汉外国语学校小学部三六班--仁爱学堂")
+
 mypen = turtle.Pen()
+mycolor=["black","red","pink","blue","yellow"]
 
-print(u"武汉外国语学校小学部三六班")
+forword_step = 150
+turn_degree = 90
 
-#for each in range(4):
-    #mypen.forward(180);
-#    time.sleep(1);
-    #mypen.right(90);
-
-mypen.right(180)
-mypen.forward(90)
-
-random.seed(10);
 for loop in range(150):
-    forword_step = random.randint(1, 50);
-    mypen.forward(forword_step);
-    turn_degree = random.randint(1, 90);
-    mypen.right(turn_degree % 90);
-
-time.sleep(1);
+    mypen.pencolor(mycolor[random.randint(0, len(mycolor)-1)])
+    mypen.forward(forword_step)
+    forword_step = forword_step - 1
+    mypen.right(turn_degree)
