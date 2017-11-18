ef2d97407d36036f41b4f55427f8f201e68f16e4
weichaojie <weichaojie@huawei.com>
2017-11-18 19:09:32 +0800

    修正内容
    Signed-off-by: weichaojie <weichaojie@huawei.com>

--- myturtle/luojinrong.py
+++ myturtle/luojinrong.py
@@ -2,7 +2,6 @@
 import turtle
 import time
 import random
-import math
 
 print(u"武汉外国语学校小学部三六班--仁爱学堂")
 
@@ -14,6 +13,9 @@ turn_degree = 90
 
 for loop in range(150):
     mypen.pencolor(mycolor[random.randint(0, len(mycolor)-1)])
-    mypen.forward(forword_step)
-    forword_step = forword_step - 1
-    mypen.right(turn_degree)
+    if (forword_step > 0):
+        mypen.forward(forword_step)
+        forword_step = forword_step - 5
+        mypen.right(turn_degree)
+        
+time.sleep(10)
