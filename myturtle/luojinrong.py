1ddfdfe207487d7e281568e588dbdbb4d2a5c52a
weichaojie <weichaojie@huawei.com>
2017-11-18 15:47:35 +0800

    更新乐龟的画图方法 Signed-off-by: weichaojie <weichaojie@huawei.com>

--- myturtle/luojinrong.py
+++ myturtle/luojinrong.py
@@ -1,14 +1,26 @@
+#coding=utf-8
 import turtle
 import time
+import random
+import math
 
 mypen = turtle.Pen()
 
-print("luojinrong")
+print(u"武汉外国语学校小学部三六班")
 
-for each in range(4):
-    mypen.forward(180);
-    time.sleep(1);
-    mypen.right(90);
+#for each in range(4):
+    #mypen.forward(180);
+#    time.sleep(1);
+    #mypen.right(90);
 
 mypen.right(180)
 mypen.forward(90)
+
+random.seed(10);
+for loop in range(150):
+    forword_step = random.randint(1, 50);
+    mypen.forward(forword_step);
+    turn_degree = random.randint(1, 90);
+    mypen.right(turn_degree % 90);
+
+time.sleep(1);
