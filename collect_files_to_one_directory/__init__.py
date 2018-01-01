fd4471e8d85fc0781bbcfded76e6cf173c5ec57d
weichaojie <18971302695@189.cn>
2018-01-01 10:18:41 +0800

    新增文件遍历的方法. Signed-off-by: weichaojie <weichaojie@huawei.com>
    Signed-off-by: weichaojie <weichaojie@huawei.com>

---
+++ collect_files_to_one_directory/__init__.py
@@ -0,0 +1,28 @@
+import os
+import time
+
+target_walk_path = "D:\\My Documents\\下载"
+
+global_files_list = []
+
+
+def walk(path):
+    global_files_list.clear()
+    if not os.path.exists(path):
+        return -1
+    for root, dirs, names in os.walk(path):
+        for filename in names:
+            if os.path.splitext(filename)[1] == '.rmvb':
+                full_file_path = os.path.join(root, filename)
+                global_files_list.append(full_file_path)
+                print(full_file_path)  # 路径和文件名连接构成完整路径
+
+
+while (1):
+
+    walk(target_walk_path)
+    time.sleep(1)
+
+    if len(global_files_list) == 0:
+        print("休眠15秒")
+        time.sleep(15)
