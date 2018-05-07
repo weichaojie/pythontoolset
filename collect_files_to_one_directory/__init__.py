cd55793fbbea680e3d57e97c3f38830a597505e7
weichaojie <weichaojie@huawei.com>
2018-05-08 01:47:01 +0800

    使用tuple的列表存储文件全路径和文件名，将文件移动到指定的路径下保存 Signed-off-by: weichaojie <weichaojie@huawei.com>
    Signed-off-by: weichaojie <weichaojie@huawei.com>

--- collect_files_to_one_directory/__init__.py
+++ collect_files_to_one_directory/__init__.py
@@ -1,9 +1,14 @@
 import os
+import shutil
 import time
 
-target_walk_path = "D:\\My Documents\\下载"
-
-global_files_list = []
+# search_walk_path = "D:\\My Documents\\下载"
+search_walk_path = "C:\codes\webtoolset"
+target_save_path = "C:\\temp"
+# search_file_type = '.rmvb'
+search_file_type = '.html'
+sleep_time = 180
+global_files_list = ["", ""]
 
 
 def walk(path):
@@ -12,17 +17,29 @@ def walk(path):
         return -1
     for root, dirs, names in os.walk(path):
         for filename in names:
-            if os.path.splitext(filename)[1] == '.rmvb':
+            if os.path.splitext(filename)[1] == search_file_type:
+                # 路径和文件名连接构成完整路径
                 full_file_path = os.path.join(root, filename)
-                global_files_list.append(full_file_path)
-                print(full_file_path)  # 路径和文件名连接构成完整路径
+                # 将符合条件的文件的全路径记录保存起来
+                global_files_list.append((full_file_path, filename))
+                print(full_file_path, filename)
 
+
+def move_files_to_directory(path_name_list, target_directory):
+    for (fullpath, name) in path_name_list:
+        new_file_full_path = target_directory + "\\" + name
+        shutil.move(fullpath, new_file_full_path)
+        print(new_file_full_path)
 
 while (1):
 
-    walk(target_walk_path)
-    time.sleep(1)
+    # 循环遍历扫描目录
+    walk(search_walk_path)
+    time.sleep(2)
 
     if len(global_files_list) == 0:
-        print("休眠15秒")
-        time.sleep(15)
+        print("遍历目录后发现没有合适的文件后进行休眠", sleep_time, "秒，等待有文件准备好")
+        time.sleep(sleep_time)
+    else:
+        # 有合适的文件时进行文件移动操作
+        move_files_to_directory(global_files_list, target_save_path)
