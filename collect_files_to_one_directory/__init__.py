7b50c6e8ad470090502ac92d53f724fdd1c3df63
weichaojie <18971302695@189.cn>
2018-05-09 03:20:02 +0800

    Merge remote-tracking branch 'origin/master' 函数重构，使用局部变量进行传递。 Signed-off-by: weichaojie <weichaojie@huawei.com>
    Signed-off-by: w00168437 <weichaojie@huawei.com>

--- collect_files_to_one_directory/__init__.py
+++ collect_files_to_one_directory/__init__.py
@@ -2,26 +2,27 @@ import os
 import shutil
 import time
 
-search_walk_path = "D:\\My Documents\\下载"
-#search_walk_path = "C:\codes\webtoolset"
-target_save_path = "\\\\weichaojie2695\\video\\电影"
-search_file_type = '.rmvb'
-#search_file_type = '.html'
-sleep_time = 180
+g_search_walk_path = "D:\\My Documents\\下载"
+# g_search_walk_path = "C:\codes\webtoolset"
+g_target_save_path = "\\\\weichaojie2695\\video\\电影"
+g_search_file_type = '.rmvb'
+# g_search_file_type = '.html'
+g_sleep_time = 180
 global_files_list = ["", ""]
 
 
-def walk(path):
-    global_files_list.clear()
+def walk(path, search_file_type, files_list):
+    files_list.clear()
     if not os.path.exists(path):
         return -1
+
     for root, dirs, names in os.walk(path):
         for filename in names:
             if os.path.splitext(filename)[1] == search_file_type:
                 # 路径和文件名连接构成完整路径
                 full_file_path = os.path.join(root, filename)
                 # 将符合条件的文件的全路径记录保存起来
-                global_files_list.append((full_file_path, filename))
+                files_list.append((full_file_path, filename))
                 print(full_file_path, filename)
 
 
@@ -34,12 +35,12 @@ def move_files_to_directory(path_name_list, target_directory):
 while (1):
 
     # 循环遍历扫描目录
-    walk(search_walk_path)
+    walk(g_search_walk_path, g_search_file_type, global_files_list)
     time.sleep(2)
 
     if len(global_files_list) == 0:
-        print("遍历目录后发现没有合适的文件后进行休眠", sleep_time, "秒，等待有文件准备好")
-        time.sleep(sleep_time)
+        print("遍历目录后发现没有合适的文件后进行休眠", g_sleep_time, "秒，等待有文件准备好")
+        time.sleep(g_sleep_time)
     else:
         # 有合适的文件时进行文件移动操作
-        move_files_to_directory(global_files_list, target_save_path)
+        move_files_to_directory(global_files_list, g_target_save_path)
