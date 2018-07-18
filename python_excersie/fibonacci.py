ddf58ba3fcda06a4744c12e8b42c87e35da20ff3
weichaojie <18971302695@189.cn>
2018-07-18 03:23:49 +0000

    Update fibonacci.py
    1）add assert judgement
    2）modify log method

--- python_excersie/fibonacci.py
+++ python_excersie/fibonacci.py
@@ -18,6 +18,9 @@
 # 使用循环的方法计算斐波那契数列
 # 循环方法相比递归求法的效率要高1000倍以上，数字越大提升效率越高
 def get_fibonacci_number(n):
+    assert isinstance(n, (int, long))
+    assert n >= 1
+    
     if n == 1:
         return 1
     elif n == 2:
@@ -34,13 +37,7 @@ def get_fibonacci_number(n):
             next = previous + current
             previous = current
             current = next
-    else:
-        try:
-            print("函数输入异常")
-        except:
-            return 0
-
 
 for i in range(100):
     fibonacci = get_fibonacci_number(i + 1)
-    print("第%d个数为：%d" % (i + 1, fibonacci))
+    print("The %d fibonacci number is ：%d" % (i + 1, fibonacci))
